113e6b3d99ef8373fb343980340bbc87214ffb85
Nicolás Martínez González <n.gonzalezm@udc.es>
2022-05-31 14:11:17 +0200

    segundo intento

---
+++ quol/src/main/java/quol/parser/App.java
@@ -0,0 +1,15 @@
+package quol.parser;
+
+import javax.sound.sampled.AudioSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
