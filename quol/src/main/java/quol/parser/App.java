8dce90fff03a5103d6def9a38ad9af04c53bc5cf
Nicolás Martínez González <n.gonzalezm@udc.es>
2022-05-31 16:56:49 +0200

    xd

--- quol/src/main/java/quol/parser/App.java
+++ quol/src/main/java/quol/parser/App.java
@@ -1,6 +1,8 @@
 package quol.parser;
 
-import javax.sound.sampled.AudioSystem;
+//import javax.sound.sampled.AudioSystem;
+//import javax.sound.sampled.Mixer;
+import javax.sound.sampled.*;
 
 /**
  * Hello world!
@@ -11,5 +13,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for(Mixer.Info i : AudioSystem.getMixerInfo()) {
+        	System.out.println(i.getName()+"\t"+i.hashCode());
+        }
     }
 }
