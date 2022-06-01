d9d59b35ad95037090d0f9c9aef04fd49ab3dc4c
Nicolás Martínez González <n.gonzalezm@udc.es>
2022-06-01 13:07:44 +0200

    fase previa de identificación de drivers/mixers/lines

--- quol/src/main/java/quol/parser/Parser.java
+++ quol/src/main/java/quol/parser/Parser.java
@@ -2,17 +2,27 @@ package quol.parser;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Mixer;
+import javax.sound.sampled.Port;
 //import javax.sound.sampled.*;
 
 public class Parser {
 	public static String mainMixerName = "Controlador primario de sonido";
     public static void main( String[] args){
     	Mixer mainMixer = null;
+    	Port line = null;
         for(Mixer.Info i : AudioSystem.getMixerInfo()) {
         	if (i.getName().equals(mainMixerName)) {
         		mainMixer = AudioSystem.getMixer(i);
         	}
         	System.out.println(i.getName()+"\t"+i.hashCode());
+        }
+        if (AudioSystem.isLineSupported(Port.Info.LINE_OUT)) {
+            try {
+                line = (Port) AudioSystem.getLine(
+                    Port.Info.LINE_OUT);
+            } catch (Exception e) {
+            	e.printStackTrace();
+            }
         }
     }
 }
